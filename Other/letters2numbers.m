5555165ced9e348a6e97b2755bb5a22184316c89
Hugh Signoriello <53002705+hughmancoder@users.noreply.github.com>
2021-10-11 22:21:08 +1030

    cleaned up code

--- Other/letters2numbers.m
+++
@@ -1,8 +0,0 @@
-
-letters2numbers('hello');
-
-
-function    num = letters2numbers( word )
-    asc = double( upper(word) );    % http://www.asciitable.com/
-    num = 26 + double('A') - asc;   % simple arithmetic
-end
